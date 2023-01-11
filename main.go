46a47548cf3f4507e533d7c253f10e37dc9d8c86
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-01-11 03:13:26 +0100

    add all the needed essentials from slacker

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/shomali11/slacker"
+)
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Event")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+
+func main() {
+
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-4642112027856-4647051681728-a9640nXlpME3XyeA7oaO1MBX")
+	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A04J4L3U4JJ-4646994360352-0b9be2e5d6082851d0fb6d0afcdc5e80c9b60c9b12527631e6262fcefb607d48")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("My yob is <year>", &slacker.CommandDefinition{
+		Description: "year of birth calculator",
+		Examples:    []string{"my yob is 2020"},
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+			if err != nil {
+				println("error")
+			}
+			age := time.Now().Year() - yob
+			r := fmt.Sprintf("age is %d", age)
+			response.Reply(r)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
